1359eb296827036adb64f250fe9e497bfd255dbd
Max <accfortimersite@mail.ru>
2024-06-21 22:42:04 +0300

    features

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,10 @@ import { useEffect, useState } from 'react';
 
 function getData(data: string) {
   const splittedData: Array<{ [key: string]: string }> = [];
+  // eslint-disable-next-line no-control-regex
   const newData = data.split(/\d+.	/).slice(1);
 
-  for (let item of newData) {
+  for (const item of newData) {
     const [header, answer] = item.replace('\n', 'DELETE').split('DELETE');
     splittedData.push({ [header]: answer });
   }
@@ -16,16 +17,71 @@ function getData(data: string) {
 }
 
 function App() {
+  const [opened, setOpened] = useState<boolean>(true);
   const [info, setInfo] = useState<Array<{ [key: string]: string }>>();
+  const [inputValue, setInputValue] = useState('');
 
   useEffect(() => {
     setInfo(getData(data));
   }, []);
 
+  function handleClickKeys(e: React.SyntheticEvent) {
+    const target = e.target as HTMLElement;
+    if (target.tagName === 'LI') {
+      setInputValue((prev) => prev + target.innerHTML);
+    }
+  }
+
+  function handleClickDel() {
+    setInputValue((prev) => prev.slice(0, -1));
+  }
+
   if (!info) return <div>Нет информации</div>;
 
   return (
     <>
+      {opened && (
+        <>
+          <div className='input_div'>{inputValue}</div>
+          <img className='delete' src={Delete} alt='delete' onClick={handleClickDel} />
+          <ul className='wrapper' onClick={(e) => handleClickKeys(e)}>
+            <li className='but'>A</li>
+            <li className='but'>Б</li>
+            <li className='but'>В</li>
+            <li className='but'>Г</li>
+            <li className='but'>Д</li>
+            <li className='but'>Е</li>
+            <li className='but'>Ё</li>
+            <li className='but'>Ж</li>
+            <li className='but'>З</li>
+            <li className='but'>И</li>
+            <li className='but'>Й</li>
+            <li className='but'>К</li>
+            <li className='but'>Л</li>
+            <li className='but'>М</li>
+            <li className='but'>Н</li>
+            <li className='but'>О</li>
+            <li className='but'>П</li>
+            <li className='but'>Р</li>
+            <li className='but'>С</li>
+            <li className='but'>Т</li>
+            <li className='but'>У</li>
+            <li className='but'>Ф</li>
+            <li className='but'>Х</li>
+            <li className='but'>Ц</li>
+            <li className='but'>Ч</li>
+            <li className='but'>Ш</li>
+            <li className='but'>Щ</li>
+            <li className='but'>Ы</li>
+            <li className='but'>Ъ</li>
+            <li className='but'>Э</li>
+            <li className='but'>Ь</li>
+            <li className='but'>Ю</li>
+            <li className='but'>Я</li>
+          </ul>
+          <button className='enter'>найти</button>
+        </>
+      )}
       <div className='main_info'>
         {info.map((item, id) => {
           const [key, value] = Object.entries(item)[0];
@@ -37,46 +93,6 @@ function App() {
             </article>
           );
         })}
-      </div>
-      <div className='input_wrapper'>
-        <div id='input_div'></div>
-        <button className='enter'>найти</button>
-        <ul className='wrapper'>
-          <img src={Delete} alt='стереть' className='delete' />
-          <li className='but'>A</li>
-          <li className='but'>Б</li>
-          <li className='but'>В</li>
-          <li className='but'>Г</li>
-          <li className='but'>Д</li>
-          <li className='but'>Е</li>
-          <li className='but'>Ё</li>
-          <li className='but'>Ж</li>
-          <li className='but'>З</li>
-          <li className='but'>И</li>
-          <li className='but'>Й</li>
-          <li className='but'>К</li>
-          <li className='but'>Л</li>
-          <li className='but'>М</li>
-          <li className='but'>Н</li>
-          <li className='but'>О</li>
-          <li className='but'>П</li>
-          <li className='but'>Р</li>
-          <li className='but'>С</li>
-          <li className='but'>Т</li>
-          <li className='but'>У</li>
-          <li className='but'>Ф</li>
-          <li className='but'>Х</li>
-          <li className='but'>Ц</li>
-          <li className='but'>Ч</li>
-          <li className='but'>Ш</li>
-          <li className='but'>Щ</li>
-          <li className='but'>Ы</li>
-          <li className='but'>Ъ</li>
-          <li className='but'>Э</li>
-          <li className='but'>Ь</li>
-          <li className='but'>Ю</li>
-          <li className='but'>Я</li>
-        </ul>
       </div>
     </>
   );
